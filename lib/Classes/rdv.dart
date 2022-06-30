9310e3ccc2406c0d43747fe6d46932b79f449b32
DJEBABRIA Hiba <59914426+hibadjebabria169@users.noreply.github.com>
2022-06-30 16:53:09 +0200

    Update rdv.dart

--- lib/Classes/rdv.dart
+++ lib/Classes/rdv.dart
@@ -1,13 +1,15 @@
 class RendezVous {
   int idRdv;
   String dateRdv;
+  String locationFrom;
+  String locationTo;
+  int idPatient;
+  int idOperateur;
   String typeRdv;
   double longDep;
-  double longDes;
   double latDep;
+  double longDes;
   double latDes;
-  int NSS;
-  int idOperateur;
 
   RendezVous(
       {required this.idRdv,
@@ -18,18 +20,22 @@ class RendezVous {
       required this.latDep,
       required this.latDes,
       required this.idOperateur,
-      required this.NSS});
+      required this.idPatient,
+      required this.locationFrom,
+      required this.locationTo});
 
   Map<String, dynamic> toJson() => {
         'idrdv': idRdv,
         'daterdv': dateRdv,
         'idoperateur': idOperateur,
-        'nss': NSS,
         'typerdv': typeRdv,
         'longdep': longDep,
         'longdes': longDes,
         'latdep': latDep,
-        'latdes': latDes
+        'latdes': latDes,
+        'idpatient': idPatient,
+        'locationfrom': locationFrom,
+        'locationto': locationTo,
       };
 
   static RendezVous fromJson(Map<String, dynamic> json) {
@@ -38,8 +44,10 @@ class RendezVous {
       dateRdv: json['dateRdv'],
       typeRdv: json['typeRdv'],
       idOperateur: json['idOperateur'],
-      NSS: json['NSS'],
       longDep: json['longDep'],
+      idPatient: json['idPatient'],
+      locationFrom: json['locationFrom'],
+      locationTo: json['locationTo'],
       longDes: json['longDes'],
       latDep: json['latDes'],
       latDes: json['latDes'],
